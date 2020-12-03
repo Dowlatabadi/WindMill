592476091b3b889b5c600dccf3385231a1d4c0eb
m.dowlatabadi.ce@gmail.com <m.dowlatabadi.ce@gmail.com>
2020-12-03 06:21:47 +0330

    blur transition works

--- Assets/scripts/Scene_manager.cs
+++ Assets/scripts/Scene_manager.cs
@@ -1,35 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Classes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Classes;
+
 public class Scene_manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
     }
-void Awake(){
 
-	Screen.sleepTimeout = SleepTimeout.NeverSleep;
-}
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
     public void Scene_Playground()
     {
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
+
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game1");
     }
 
     public void Scene_Selection()
     {
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Selection");
     }
 
     public void Scene_About()
     {
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
+
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("About");
     }
 
     public void Scene_Intro()
     {
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
+
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("intro");
     }
 
@@ -38,22 +48,27 @@ void Awake(){
         Application.Quit();
     }
 
-   public void Scene_LastLvlPlay()
+    public void Scene_LastLvlPlay()
     {
-		PlayerPrefs.SetInt("temp_lvl_num", Camera.main.GetComponent<save_manager>().get_progress_lvl());
-		Debug.Log("saved last: "+ Camera.main.GetComponent<save_manager>().get_progress_lvl());
+        PlayerPrefs
+            .SetInt("temp_lvl_num",
+            Camera.main.GetComponent<save_manager>().get_progress_lvl());
+        Debug
+            .Log("saved last: " +
+            Camera.main.GetComponent<save_manager>().get_progress_lvl());
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
+
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game1");
-        
     }
 
- public void Scene_Playground_LevelParameters(
-        int Lvl_num
-    )
+    public void Scene_Playground_LevelParameters(int Lvl_num)
     {
         PlayerPrefs.SetInt("temp_lvl_num", Lvl_num);
-      
+        Camera.main.GetComponent<loading_manager>().fade_in_blur();
+
         UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game1");
     }
+
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/scripts/loading_manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperBlur;
+
+public class loading_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SuperBlurFast SuperBlurFastScript;
+
+    void Start()
+    {
+        SuperBlurFastScript = GetComponent<SuperBlurFast>();
+		fade_out_blur();
+    }
+
+    public void fade_out_blur()
+    {
+		SuperBlurFastScript.enabled=true;
+
+        SuperBlurFastScript.interpolation = 1f;
+        final_interpolation = 0f;
+        speed_factor=7;
+
+        animating = true;
+		
+    }
+
+    public void fade_in_blur()
+    {
+		SuperBlurFastScript.enabled=true;
+
+        SuperBlurFastScript.interpolation = 0;
+        final_interpolation = 1f;
+        speed_factor=2;
+		animating = true;
+
+    }
+public int speed_factor;
+    bool animating = false;
+
+   
+
+    float final_interpolation = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animating)
+        {
+            SuperBlurFastScript.interpolation =
+                Mathf
+                    .Lerp(SuperBlurFastScript.interpolation,
+                    final_interpolation,
+                    Time.deltaTime*speed_factor);
+
+            if (
+                Mathf
+                    .Abs(SuperBlurFastScript.interpolation -
+                    final_interpolation) <
+                .01
+            )
+            {
+                animating = false;
+                SuperBlurFastScript.interpolation = final_interpolation;
+				if (final_interpolation<.1f)
+				{
+					SuperBlurFastScript.enabled=false;
+				}
+            }
+        }
+    }
+}
